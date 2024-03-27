77ced5f09bfa76eb3f9731bb7e47ff8ced99da7e
bitsen <bitsen03@example.ru>
2024-03-27 17:17:27 +0300

    fix

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,32 +1,32 @@
-// const { test, expect } = require('@playwright/test');
+const { test, expect } = require('@playwright/test');
 
-// test('Проверка текстов интерфейса RSS', async ({ page }) => {
-//   // Переходим на вашу веб-страницу
-//   const localServerUrl = 'http://localhost:8080';
+test('Проверка текстов интерфейса RSS', async ({ page }) => {
+  // Переходим на вашу веб-страницу
+  const localServerUrl = 'http://localhost:8080';
 
-//   await page.goto(localServerUrl);
+  await page.goto(localServerUrl);
 
-//   await page.waitForLoadState('domcontentloaded');
-//   // Предположим, что у вас есть логика, которая загружает, проверяет и отображает тексты интерфейса.
+  await page.waitForLoadState('domcontentloaded');
+  // Предположим, что у вас есть логика, которая загружает, проверяет и отображает тексты интерфейса.
 
-//   // Проверка успешной загрузки RSS
-//   await expect(page).toHaveText('.feedback', 'Тексты интерфейса: RSS успешно загружен');
+  // Проверка успешной загрузки RSS
+  await expect(page).toHaveText('.feedback', 'Тексты интерфейса: RSS успешно загружен');
 
-//   // Проверка, что RSS уже существует
-//   await expect(page).toHaveText('.feedback', 'Тексты интерфейса: RSS уже существует');
+  // Проверка, что RSS уже существует
+  await expect(page).toHaveText('.feedback', 'Тексты интерфейса: RSS уже существует');
 
-//   // Проверка, что текст не пустой
-//   await expect(page).toHaveText('.feedback', 'Тексты интерфейса: Не должно быть пустым');
+  // Проверка, что текст не пустой
+  await expect(page).toHaveText('.feedback', 'Тексты интерфейса: Не должно быть пустым');
 
-//   // Проверка, что ссылка является валидным URL
-//   await expect(page).toHaveText('.feedback', 'Тексты интерфейса: Ссылка должна быть валидным URL');
+  // Проверка, что ссылка является валидным URL
+  await expect(page).toHaveText('.feedback', 'Тексты интерфейса: Ссылка должна быть валидным URL');
 
-//   // Проверка, что ресурс не содержит валидный RSS
-//   await expect(page).toHaveText('.feedback', 'Тексты интерфейса: Ресурс не содержит валидный RSS');
+  // Проверка, что ресурс не содержит валидный RSS
+  await expect(page).toHaveText('.feedback', 'Тексты интерфейса: Ресурс не содержит валидный RSS');
 
-//   // Проверка, что текст "Просмотр" отображается
-//   await expect(page).toHaveText('.feedback', 'Просмотр');
+  // Проверка, что текст "Просмотр" отображается
+  await expect(page).toHaveText('.feedback', 'Просмотр');
 
-//   // Проверка, что текст об ошибке сети отображается
-//   await expect(page).toHaveText('.feedback', 'Ошибка сети');
-// });
+  // Проверка, что текст об ошибке сети отображается
+  await expect(page).toHaveText('.feedback', 'Ошибка сети');
+});
